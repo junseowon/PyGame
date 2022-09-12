1274a70ddca136a1b564be87224beb72abaaeaa9
junseowon <junseowon@gmail.com>
2022-09-12 14:18:33 +0900

    update_game_images_font

--- Game/button_UI.py
+++ Game/button_UI.py
@@ -1,19 +1,12 @@
 import pygame
 
 class Button():
-    def __init__(self, input_image, x_pos, y_pos, image_width, image_height, input_image_act, act_x_pos, act_y_pos, action = None):
-        self.input_image = input_image
-        self.x_pos = x_pos
-        self.y_pos = y_pos
+    def __init__(self, input_screen, input_image, x_pos, y_pos, image_width, image_height, input_image_act, act_x_pos, act_y_pos, action = None):
         mouse_pos = pygame.mouse.get_pos()
         mouse_press = pygame.mouse.get_pressed()
         if x_pos + image_width > mouse_pos[0] > x_pos and y_pos + image_height > mouse_pos[1] > y_pos:
-            self.display.blit(input_image_act,(act_x_pos, act_y_pos))
+            input_screen.blit(input_image_act,(act_x_pos, act_y_pos))
             if mouse_press[0] and action is not None:
                 action()
         else:
-            self.display.blit(input_image, (x_pos, y_pos))
-
-    def update(self, display):
-        self.display = display
-        display.blit(self.input_image, (self.x_pos, self.y_pos))
+            input_screen.blit(input_image, (x_pos, y_pos))
--- Game/main_menu.py
+++ Game/main_menu.py
@@ -1,12 +1,53 @@
 import pygame
+from load_images import *
+from button_UI import *
 
 pygame.init()
 
+#간단한 색상 정의(상수)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-BACKGROUND_WIDTH = 1280
-BACKGROUND_HEIGHT = 720
+#게임 창 크기(상수)
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
-background_size = pygame.display.set_mode((BACKGROUND_WIDTH,BACKGROUND_HEIGHT))
-pygame.display.set_caption("친절한 진철씨")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+fps = pygame.time.Clock()
+
+howtoplay_button = pygame.image.load("image/buttons_UI/howtoplay_button.png")
+howtoplay_button_act = pygame.image.load("image/buttons_UI/howtoplay_button_act.png")
+
+def main_menu():
+    pygame.display.set_caption("menu")
+    background(screen, 0, 0)
+    headline(screen, "휴식이 없는 나에게", BLACK, 0, 0, 80)
+    headline(screen, "진정한 휴식이 뭔지 알려주는", BLACK, 0, 80, 80)
+    headline(screen, "감동 깊은 게임", BLACK, 0, 160, 80)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        Button(screen, howtoplay_button, 512, 420, 256, 144, howtoplay_button_act, 512,420, Explain_game)
+
+        pygame.display.update()
+        fps.tick(60)
+
+def Explain_game():
+    screen.fill(BLACK)
+    headline(screen, "나가", RED, 0, 0, 680)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        pygame.display.update()
+
+main_menu()
