91d7dde3ab05fd59722602cfefca7c3267193bd1
junseowon <junseowon@gmail.com>
2022-09-12 10:10:50 +0900

    first commit

---
+++ Game/main_menu.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+
+BACKGROUND_WIDTH = 1280
+BACKGROUND_HEIGHT = 720
+
+background_size = pygame.display.set_mode((BACKGROUND_WIDTH,BACKGROUND_HEIGHT))
+pygame.display.set_caption("친절한 진철씨")
+
